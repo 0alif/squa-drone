1d8d7f6d79e94b8d56a791017f28d5068128b2a1
0alif <alifhossain003@gmail.com>
2021-11-12 14:54:09 +0600

    extra

---
+++ src/Pages/Home/Drone/Drone.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import pic1 from '../../../images/drone-white.png';
+import pic2 from '../../../images/drone-black.png';
+import './Drone.css';
+
+const Drone = () => {
+    return (
+        <div>
+            <div className="row m-0 py-5">
+                <div className="col-sm-12 col-md-6">
+                    <img className="img-fluid" src={pic1} alt="" />
+                </div>
+                <div className="col-sm-12 col-md-6">
+                    <div className="drone">
+                        <h1>We Are Drone Pilots</h1>
+                        <p>The most innovate feature is probably its advanced controls. Using simple hand gestures, you can take off, tell the drone to follow you and fly hi-quality drones.</p>
+                        <h5>FASTER THAN EVER</h5>
+                        <p>Our drone services provide you with a skilled UAV pilot that will provide aerial drone videography.</p>
+                    </div>
+                </div>
+            </div>
+            <div className="row m-0 pb-5">
+                <div className="col-sm-12 col-md-6">
+                    <div className="drone">
+                        <h1>We Use The Best Drones & HD Cameras</h1>
+                        <p>New intelligent flight mode where drone flies along</p>
+                        <p>Proactively envisioned multimedia based expertise and cross-media growth strategies. Seamlessly visualize quality intellectual capital without superior collaboration and idea-sharing.</p>
+                        <h5>PHOTOGRAPHY</h5>
+                        <p>Efficiently unleash cross-media information without cross-media value. Quickly maximize timely deliverables for real-time schemas.</p>
+                    </div>
+                </div>
+                <div className="col-sm-12 col-md-6">
+                    <img className="img-fluid" src={pic2} alt="" />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Drone;
--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -2,14 +2,18 @@ import React from 'react';
 import Footer from '../../Sheared/Footer/Footer';
 import Navigation from '../../Sheared/Navigation/Navigation';
 import Banner from '../Banner/Banner';
+import Drone from '../Drone/Drone';
 import Product from '../Product/Product';
+import Reviews from '../Reviews/Reviews';
 
 const Home = () => {
     return (
         <div>
             <Navigation></Navigation>
             <Banner></Banner>
+            <Drone></Drone>
             <Product></Product>
+            <Reviews></Reviews>
             <Footer></Footer>
         </div>
     );
