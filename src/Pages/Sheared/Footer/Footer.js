ea5a83988ddc92709d6cf5cc37a0905baa5c1c03
0alif <alifhossain003@gmail.com>
2021-11-12 12:08:25 +0600

    footer

--- src/Pages/Sheared/Footer/Footer.js
+++ src/Pages/Sheared/Footer/Footer.js
@@ -12,16 +12,16 @@ const Footer = () => {
                     <p>Objectively innovate empowered manufactured products whereas parallel platforms. Holisticly predominate extensible testing. Objectively innovate empowered manufactured products whereas parallel platforms. Holisticly predominate extensible testing.</p>
                 </div>
                 <div className="col-sm-12 col-md-2">
-                    <SocialIcon className="ms-3 my-5" url="https://twitter.com" />
-                    <SocialIcon className="ms-3 my-5" url="https://facebook.com" />
-                    <SocialIcon className="ms-3 my-5" url="https://linkedin.com" />
-                </div>
-                <div className="col-sm-12 col-md-2">
                     <h5>Categories</h5>
                     <p>Home</p>
                     <p>About Us</p>
                     <p>Service</p>
                     <p>News</p>
+                </div>
+                <div className="col-sm-12 col-md-2">
+                    <SocialIcon className="ms-3 my-5" url="https://twitter.com" />
+                    <SocialIcon className="ms-3 my-5" url="https://facebook.com" />
+                    <SocialIcon className="ms-3 my-5" url="https://linkedin.com" />
                 </div>
                 <div className="col-sm-12 col-md-4">
                     <img className="img-fluid" src={map} alt="" />
