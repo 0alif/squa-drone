3dcbf5a80f49e9bc45fd151985999b57a76ceafa
0alif <alifhossain003@gmail.com>
2021-11-12 12:05:42 +0600

    banner

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import bg from '../../../images/hero.jpg';
+import './Banner.css';
+
+const heroImg = {
+    backgroundImage: `url(${bg})`,
+    "height": "100vh",
+    "width": "100%",
+    "backgroundPosition": "center",
+    "backgroundRepeat": "noRepeat",
+    "backgroundSize": "cover"
+}
 
 const Banner = () => {
     return (
-        <div>
-            <h2>This is banner</h2>
+        <div className="m-0">
+            <div className="hero-container" style={heroImg}>
+                <div className="hero">
+                    <h1>We Can Make Your <br />
+                        Celebration <span>Marvelous</span></h1>
+                    <p>While they might seem like toys, a high-quality drone is a serious investment. We've flown plenty and there are a lot of good performers that we use.</p>
+                    <button className="btn hero-btn rounded-pill">Get Started</button>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Pages/Sheared/Footer/Footer.js
+++ src/Pages/Sheared/Footer/Footer.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import { SocialIcon } from 'react-social-icons';
+import map from '../../../images/map.png';
+import './Footer.css';
 
 const Footer = () => {
     return (
-        <div>
-            <h2>Most expensive footer</h2>
+        <div className="footer">
+            <div className="row m-0">
+                <div className="col-sm-12 col-md-4">
+                    <h5>About Us</h5>
+                    <p>Objectively innovate empowered manufactured products whereas parallel platforms. Holisticly predominate extensible testing. Objectively innovate empowered manufactured products whereas parallel platforms. Holisticly predominate extensible testing.</p>
+                </div>
+                <div className="col-sm-12 col-md-2">
+                    <SocialIcon className="ms-3 my-5" url="https://twitter.com" />
+                    <SocialIcon className="ms-3 my-5" url="https://facebook.com" />
+                    <SocialIcon className="ms-3 my-5" url="https://linkedin.com" />
+                </div>
+                <div className="col-sm-12 col-md-2">
+                    <h5>Categories</h5>
+                    <p>Home</p>
+                    <p>About Us</p>
+                    <p>Service</p>
+                    <p>News</p>
+                </div>
+                <div className="col-sm-12 col-md-4">
+                    <img className="img-fluid" src={map} alt="" />
+                </div>
+            </div>
         </div>
     );
 };
